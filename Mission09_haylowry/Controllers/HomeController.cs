60d03d4d000b8b0d4a988e450fa14866d214df89
haylowry <haylow77@gmail.com>
2023-03-01 22:00:19 -0700

    Add project files.

---
+++ Mission09_haylowry/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_haylowry.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_haylowry.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookstoreContext Context { get; set; }
+        public HomeController (BookstoreContext temp)
+        {
+            Context = temp;
+        }
+        public IActionResult Index()
+        {
+            var books = Context.Books.ToList();
+            return View(books);
+        }
+
+    }
+}
